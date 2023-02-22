e1e674d682982d4665a650cce653720a511d7a0f
Cristina Hernández <achh17@gmail.com>
2023-02-22 09:03:55 -0500

    I added events and methods to create Lists and Items using 2 inputs.

--- src/Components/AddItem/AddItem.js
+++ src/Components/AddItem/AddItem.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function AddItem() {
+function AddItem({onAddItem}) {
+    
+    const [item, setItem]=useState('');
+    const [itemList, setItemList]=useState([]);
+  
+    const handleChange = (event) => {
+      setItem(event.target.value);
+    };
+  
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        const newItem = {
+          id:Date.now(),
+          name:item,
+        };
+        setItemList([...itemList, newItem]);
+        setItem('');
+      }
+    }
     return (
         <div>
-            <input type="text" placeholder = "agrega un item" />
+            <input type="text"
+            placeholder='Agrega una tarea'
+            value={item}
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            />
+            <ul>
+                {itemList.map(item => (
+                    <li key={item.id}>{item.name}</li>
+                ))}
+            </ul>
         </div>
-    );
+    )
 }
-
-
-
 
 export default AddItem;
--- src/Components/AddList/AddList.js
+++ src/Components/AddList/AddList.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React,{useState} from 'react';
 import AddItem from '../AddItem/AddItem';
 
 function AddList() {
+  //UseSatete Hook de react, 
+  const [text, setText]=useState(''); //estado para almacenar la entrada del usuaruio
+  const [list, setList]=useState([]); //estado para almacenar la lista de listas
+
+  // controlador de eventos para cuando cambia el valor del input
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  // controlador de eventos para cuando se presiona la tecla Enter en el input
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault(); //previene el comportamiento por defecto del formulario
+      const newList = { // crea un nuevo objeto para agregar a la lista de listas
+        id:Date.now(),
+        title:text,
+        items:[]
+      };
+      setList([...list, newList]); // agrega el nuevo objeto a la lista de listas
+      setText(''); // limpiamos el valor del input
+    }
+  }
+
     return (
        <div>
-         <input type="text" placeholder="agrega una Lista" />
-        <AddItem />
+         <input type="text"
+          placeholder="agrega una Lista"
+          value= {text}
+          onChange = {handleChange}
+          onKeyDown = {handleKeyDown}
+        />
+        {list.map(list =>(
+          <div key={list.id}>
+            <h3>{list.title}</h3> 
+            <AddItem/>
+          </div>
+        ))}
        </div>
     );
 }
